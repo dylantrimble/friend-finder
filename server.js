23be679b748c4e45832f6f50bc79553e9d9c6ff8
dyl trimble <dyltrimble12@gmail.com>
2019-10-15 15:20:53 -0400

    t co

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
@@ -6,19 +7,18 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+// For serving of static CSS
+app.use(express.static(__dirname + "/app/css"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require('./routing/apiRoutes')(app);
-require('./routing/htmlRoutes')(app);
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public'));
-});
-
-app.listen(PORT, function () {
-  console.log('Listening on port: ' + PORT);
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
 });
