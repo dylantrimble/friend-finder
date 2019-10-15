8b2218a5ac06b7a29b0252bad69b5660d1339ca9
dyl trimble <dyltrimble12@gmail.com>
2019-10-15 14:49:14 -0400

    got alot of the api route logic done

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Takumi",
+		photo: "https://vignette.wikia.nocookie.net/initiald/images/5/54/Takumi.jpg/revision/latest?cb=20111011185903",
+		scores: [5, 5, 2, 4, 5, 1, 1, 1, 1, 1]
+	},
+	{
+		name: "Itsuki",
+		photo: "https://rei.animecharactersdatabase.com/uploads/chars/5457-1232925368.jpg",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 5, 1]
+	},
+	{
+		name: "Keisuke",
+		photo: "https://static.carthrottle.com/workspace/uploads/posts/2018/07/189a053353ec999f5e3396e6bc26d83f.jpg",
+		scores: [1, 1, 5, 5, 2, 1, 1, 5, 2, 5]
+	},
+	{
+		name: "Bunta",
+		photo: "https://i2.wp.com/www.beastgamerkuma.com/wp-content/uploads/2015/09/Bunta-Fujiwara.jpg?fit=560%2C315&ssl=1",
+		scores: [1, 5, 1, 1, 5, 5, 1, 1, 1, 1]
+	},
+	{
+		name: "Yuichi",
+		photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/yuichi-tachibana-initial-d-first-stage-92.9.jpg",
+		scores: [1, 1, 1, 1, 1, 3, 5, 1, 1, 1]
+	}
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+  // Return all friends found in friends.js as JSON
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    // Receive user details (name, photo, scores)
+    var user = req.body;
+
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    var bestFriendIndex = 0;
+    var minimumDifference = 40;
+
+    // Probably use another for loop to build out the lofic for comparing the Minimum difference to the new user score. 
+
+
+  });
+};
